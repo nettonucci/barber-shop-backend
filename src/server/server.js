12d55bd4a56fd7e6e3e95e336ca40b5a050981e3
Gogs <gogs@fake.local>
2022-09-24 15:32:29 -0300

    Testing

--- src/server/server.js
+++ src/server/server.js
@@ -12,6 +12,6 @@ app.use(router)
 app.use(express.json())
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`)
+  console.log(`Server is running on port: ${process.env.PORT}`)
 })
 
